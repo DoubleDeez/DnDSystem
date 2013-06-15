206d3c3604e9536d089100602504d38d2f9fc31d
dadumesnil <dadumesnil@192.168.254.3>
2013-06-15 13:49:29 -0700

    EXP is now given to all active players (even unconscious)

--- dm/calls/addEXP.php
+++ dm/calls/addEXP.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	mysql_connect("$host", "$user", "$pass") or die(mysql_error());
 	mysql_select_db("$db") or die(mysql_error());
 	
-	$result = mysql_query("SELECT * FROM characters WHERE disable='0' AND hp>='0'") or die(mysql_error());
+	$result = mysql_query("SELECT * FROM characters WHERE disable='0'") or die(mysql_error());
 	
 		$numChars = mysql_num_rows($result);
 		$exp = floor(intval(clean($in->exp)) / $numChars);
